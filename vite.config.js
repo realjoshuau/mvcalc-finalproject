e644427cc17cfe2bd797eb1f770e25a22e8c120b
Josh <102478723+realjoshuau@users.noreply.github.com>
2025-04-26 21:10:38 -0700

    builds?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+    base: "/mvcalc-finalproject/",
     plugins: [tailwindcss()],
 });
